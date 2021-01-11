47e401a42f5117c019bc1757ca6e9b58d4808c6a
James <Icedshot@gmail.com>
2021-01-11 06:11:32 +0000

    derivative definitions

--- dual_value.hpp
+++ dual_value.hpp
@@ -1017,6 +1017,9 @@ using dual = dual_types::dual_v<dual_types::value>;
 using dual_complex = dual_types::dual_v<dual_types::complex<dual_types::value>>;
 using value = dual_types::value;
 
+///https://physics.stackexchange.com/questions/51915/can-one-raise-indices-on-covariant-derivative-and-products-thereof raising indices on covariant derivatives
+
+///http://web.mit.edu/edbert/GR/gr11.pdf ???
 template<typename T, typename U, int N>
 inline
 tensor<T, N, N, N> christoffel_symbols_2(const tensor<T, N, N>& metric, const std::array<U, N>& variables)
@@ -1051,7 +1054,7 @@ tensor<T, N, N, N> christoffel_symbols_2(const tensor<T, N, N>& metric, const st
 ///Du v^v
 template<typename T, typename U, int N>
 inline
-tensor<T, N, N> covariant_derivative(const vec<N, T>& v_in, const tensor<T, N, N>& metric, const std::array<U, N>& variables)
+tensor<T, N, N> covariant_derivative_raised(const vec<N, T>& v_in, const tensor<T, N, N>& metric, const std::array<U, N>& variables)
 {
     tensor<T, N, N, N> christoff = christoffel_symbols_2(metric, variables);
     tensor<T, N, N> duvv;
@@ -1074,6 +1077,61 @@ tensor<T, N, N> covariant_derivative(const vec<N, T>& v_in, const tensor<T, N, N
     return duvv;
 }
 
+///https://en.wikipedia.org/wiki/Covariant_derivative#Covariant_derivative_by_field_type
+template<typename T, typename U, int N>
+inline
+tensor<T, N, N> covariant_derivative_lowered(const vec<N, T>& v_in, const tensor<T, N, N>& metric, const std::array<U, N>& variables)
+{
+    tensor<T, N, N, N> christoff = christoffel_symbols_2(metric, variables);
+    tensor<T, N, N> lac;
+
+    for(int a=0; a < N; a++)
+    {
+        for(int c=0; c < N; c++)
+        {
+            T sum = 0;
+
+            for(int b=0; b < N; b++)
+            {
+                sum += christoff.idx(b, c, a) * v_in.v[b];
+            }
+
+            lac.idx(a, c) = v_in.v[a].differentiate(variables[c]) - sum;
+        }
+    }
+
+    return lac;
+}
+
+template<typename T, typename U, int N>
+inline
+tensor<T, N, N, N> covariant_derivative_mixed(const tensor<T, N, N>& mT, const tensor<T, N, N>& metric, const std::array<U, N>& variables)
+{
+    tensor<T, N, N, N> christoff = christoffel_symbols_2(metric, variables);
+    tensor<T, N, N, N> tabc;
+
+    for(int a = 0; a < N; a++)
+    {
+        for(int b=0; b < N; b++)
+        {
+            for(int c=0; c < N; c++)
+            {
+                T sum = 0;
+
+                for(int d=0; d < N; d++)
+                {
+                    sum += christoff.idx(a, c, d) * mT.idx(d, b) - christoff.idx(d, c, b) * mT.idx(a, d);
+                }
+
+                tabc.idx(a, b, c) = mT.idx(a, b).differentiate(variables[c]) + sum;
+            }
+        }
+    }
+
+    return tabc;
+}
+
+
 ///https://scholarworks.rit.edu/cgi/viewcontent.cgi?article=11286&context=theses 2.24
 template<typename T, typename U, int N>
 inline
@@ -1094,5 +1152,31 @@ tensor<T, N, N> metric_lie_derivative(const vec<N, T>& u, const tensor<T, N, N>&
     return lie;
 }
 
+///mixed indices for T
+template<typename T, typename U, int N>
+inline
+tensor<T, N, N> lie_derivative_mixed(const vec<N, T>& u, const tensor<T, N, N>& mT, const tensor<T, N, N>& metric, const std::array<U, N>& variables)
+{
+    tensor<T, N, N> lie;
+
+    for(int a=0; a < N; a++)
+    {
+        for(int b=0; b < N; b++)
+        {
+            T sum = 0;
+
+            for(int g=0; g < N; g++)
+            {
+                sum += u.v[g] * covariant_derivative_mixed(mT, metric, variables).idx(b, a, g) +
+                       mT.idx(g, a) * covariant_derivative_raised(u, metric, variables).idx(b, g) -
+                       mT.idx(b, g) * covariant_derivative_raised(u, metric, variables).idx(g, a);
+            }
+
+            lie.idx(b, a) = sum;
+        }
+    }
+
+    return lie;
+}
 
 #endif // DUAL2_HPP_INCLUDED
