a0b3f244025abd6fb926984f6be2a1b44178d418
20k <icedshot@gmail.com>
2022-01-09 18:24:02 +0000

    new schwarzschild

---
+++ scripts/schwarzschild.js
@@ -0,0 +1,15 @@
+function metric(t, r, theta, phi)
+{
+    var rs = 1;
+    var c = 1;
+
+    var dt = -(1 - rs / r) * c * c;
+    var dr = 1/(1 - rs / r);
+    var dtheta = r*r;
+    var dphi = r*r * CMath.sin(theta) * CMath.sin(theta);
+
+    return [dt, dr, dtheta, dphi];
+    //return [-1, 1, r * r, r * r * CMath.sin(theta) * CMath.sin(theta)];
+}
+
+metric
