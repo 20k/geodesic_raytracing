55c89201921467e3c80e4f3b815131cf83769599
20k <icedshot@gmail.com>
2022-01-09 12:15:14 +0000

    beginning of very basic javascript autodifferentiation

--- js_interop.cpp
+++ js_interop.cpp
@@ -1,12 +1,49 @@
 #include "js_interop.hpp"
 #include <toolkit/fs_helpers.hpp>
 #include <iostream>
+#include "dual_value.hpp"
 
 namespace js = js_quickjs;
 
+void san(js::value& v)
+{
+    if(!v.has("v"))
+    {
+        dual d;
+
+        js::value next(*v.vctx);
+
+        next.allocate_in_heap(d);
+
+        v.add("v", next);
+    }
+}
+
+dual& get(js::value& v)
+{
+    san(v);
+
+    return *v.get("v").get_ptr<dual>();
+}
+
+js::value to_value(js::value_context& vctx, dual in)
+{
+    js::value v(vctx);
+
+    js::value as_object(vctx);
+    as_object.allocate_in_heap(in);
+
+    v.add("v", as_object);
+
+    return v;
+}
+
 js::value add(js::value_context* vctx, js::value v1, js::value v2)
 {
-    return js::value(*vctx);
+    dual pv1 = get(v1);
+    dual pv2 = get(v2);
+
+    return to_value(*vctx, pv1 + pv2);
 }
 
 std::string js_argument_string(const std::string& script_data)
