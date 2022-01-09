72a43945cc3147b4aa05d89471b3b019122e61a2
20k <icedshot@gmail.com>
2022-01-09 14:18:19 +0000

    oh my goodness it actually worked

--- js_interop.cpp
+++ js_interop.cpp
@@ -3,6 +3,15 @@
 #include <iostream>
 #include "dual_value.hpp"
 
+js::value to_class(js::value_context& vctx, js::value in)
+{
+    js::value global = js::get_global(vctx);
+
+    const std::string name = "make_class";
+
+    return js::call_prop(global, name, in).second;
+}
+
 void san(js::value& v)
 {
     if(!v.has("v"))
@@ -17,8 +26,15 @@ void san(js::value& v)
     }
 }
 
-dual& get(js::value& v)
+dual get(js::value& v)
 {
+    if(v.is_number())
+    {
+        double val = v;
+
+        return val;
+    }
+
     san(v);
 
     return *v.get("v").get_ptr<dual>();
@@ -33,7 +49,26 @@ js::value to_value(js::value_context& vctx, dual in)
 
     v.add("v", as_object);
 
-    return v;
+    return to_class(vctx, v);
+}
+
+void construct(js::value_context* vctx, js::value js_this, js::value v2)
+{
+    if(v2.has("v"))
+    {
+        js_this.add("v", v2.get("v"));
+    }
+    else
+    {
+        double value = v2;
+
+        dual as_dual = value;
+
+        js::value val(*vctx);
+        val.allocate_in_heap(as_dual);
+
+        js_this.add("v", val);
+    }
 }
 
 js::value add(js::value_context* vctx, js::value v1, js::value v2)
@@ -49,7 +84,11 @@ js::value mul(js::value_context* vctx, js::value v1, js::value v2)
     dual pv1 = get(v1);
     dual pv2 = get(v2);
 
-    return to_value(*vctx, pv1 * pv2);
+    js::value result = to_value(*vctx, pv1 * pv2);
+
+    std::cout << (std::string)result << std::endl;
+
+    return result;
 }
 
 namespace CMath
@@ -75,6 +114,7 @@ js_function::js_function(const std::string& script_data) : vctx(nullptr, nullptr
 
     js::add_key_value(cshim, "add", js::function<add>);
     js::add_key_value(cshim, "mul", js::function<mul>);
+    js::add_key_value(cshim, "construct", js::function<construct>);
 
     js::value global = js::get_global(vctx);
 
@@ -95,5 +135,40 @@ js_function::js_function(const std::string& script_data) : vctx(nullptr, nullptr
 
 std::array<dual, 16> js_function::operator()(dual t, dual r, dual theta, dual phi)
 {
-    return std::array<dual, 16>{};
+    js::value v1 = to_value(vctx, t);
+    js::value v2 = to_value(vctx, r);
+    js::value v3 = to_value(vctx, theta);
+    js::value v4 = to_value(vctx, phi);
+
+    std::cout << (std::string)type_to_string(v2.get("v").get_ptr<dual>()->real) << std::endl;
+
+    auto [success, result] = js::call(func, v1, v2, v3, v4);
+
+    std::cout << "Res " << (std::string)result << std::endl;
+
+    if(!success)
+        throw std::runtime_error("Error in script exec " + (std::string)result);
+
+    if(!result.is_array())
+        throw std::runtime_error("Must return array");
+
+    std::vector<js::value> values = result;
+
+    if(values.size() == 4)
+    {
+        return {get(values[0]), 0, 0, 0,
+                0, get(values[1]), 0, 0,
+                0, 0, get(values[2]), 0,
+                0, 0, 0, get(values[3])};
+    }
+    else
+    {
+        if(values.size() != 16)
+            throw std::runtime_error("Must return array length of 4 or 16");
+
+        return {get(values[0]), get(values[1]), get(values[2]), get(values[3]),
+                get(values[4]), get(values[5]), get(values[6]), get(values[7]),
+                get(values[8]), get(values[9]), get(values[10]),get(values[11]),
+                get(values[12]),get(values[13]),get(values[14]),get(values[15])};
+    }
 }
--- schwarzschild.js
+++ schwarzschild.js
@@ -1,5 +1,10 @@
 function metric(t, r, theta, phi)
 {
+    t = new CMath.number(t);
+    r = new CMath.number(r);
+    theta = new CMath.number(theta);
+    phi = new CMath.number(phi);
+
     return [-1, 1, r * r, r * r * CMath.sin(theta) * CMath.sin(theta)];
 }
 
