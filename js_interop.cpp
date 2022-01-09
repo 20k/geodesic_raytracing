4d29e8baf998e4de4ed7f70efd2a1c2ee9f6cffd
20k <icedshot@gmail.com>
2022-01-09 11:47:01 +0000

    function add

--- js_interop.cpp
+++ js_interop.cpp
@@ -2,10 +2,15 @@
 #include <toolkit/fs_helpers.hpp>
 #include <iostream>
 
+namespace js = js_quickjs;
+
+js::value add(js::value_context* vctx, js::value v1, js::value v2)
+{
+    return js::value(*vctx);
+}
+
 std::string js_argument_string(const std::string& script_data)
 {
-    namespace js = js_quickjs;
-
     std::string wrapper = file::read("./number.js",file::mode::TEXT);
 
     js::value_context vctx(nullptr, nullptr);
@@ -14,6 +19,14 @@ std::string js_argument_string(const std::string& script_data)
     JS_AddIntrinsicBigDecimal(vctx.ctx);
     JS_AddIntrinsicOperators(vctx.ctx);
     JS_EnableBignumExt(vctx.ctx, true);
+
+    js::value cshim(vctx);
+
+    js::add_key_value(cshim, "add", js::function<add>);
+
+    js::value global = js::get_global(vctx);
+
+    js::add_key_value(global, "CShim", cshim);
 
     js::value result = js::eval(vctx, wrapper);
 
