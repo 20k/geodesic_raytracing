d9623ccfd801b1396827cf289327fa4e38c4c328
20k <icedshot@gmail.com>
2022-01-10 08:28:31 +0000

    jnw, but doesn't seem to be working great

---
+++ scripts/janis_newman_winicour.js
@@ -0,0 +1,33 @@
+///https://arxiv.org/pdf/1408.6041.pdf is where this formulation comes from
+function janis_newman_winicour(t, r, theta, phi)
+{
+    var r0 = 1;
+    ///mu = [1, +inf]
+    var mu = 4;
+
+    var Ar = CMath.pow((2 * r - r0 * (mu - 1)) / (2 * r + r0 * (mu + 1)), 1/mu);
+    var Br = (1/4.) * CMath.pow(2 * r + r0 * (mu + 1), (1/mu) + 1) / CMath.pow(2 * r - r0 * (mu - 1), (1/mu) - 1);
+
+    var dt = -Ar;
+    var dr = 1/Ar;
+    var dtheta = Br;
+    var dphi = Br * CMath.sin(theta) * CMath.sin(theta);
+
+    return [dt, dr, dtheta, dphi];
+
+    ///this formulation has coordinate singularities coming out of its butt
+    /*dual q = sqrt(3) * 1.1;
+    dual M = 1;
+    dual b = 2 * sqrt(M * M + q * q);
+
+    dual gamma = 2*M/b;
+
+    dual dt = -pow(1 - b/r, gamma);
+    dual dr = pow(1 - b/r, -gamma);
+    dual dtheta = pow(1 - b/r, 1-gamma) * r * r;
+    dual dphi = pow(1 - b/r, 1-gamma) * r * r * sin(theta) * sin(theta);
+
+    return {dt, dr, dtheta, dphi};*/
+}
+
+janis_newman_winicour
