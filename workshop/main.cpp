6c6514550f1650ead5024c9da44253a60b06aa97
20k <icedshot@gmail.com>
2022-01-15 14:19:41 +0000

    horrible intermediate mess of callback figuring out in the steam api

--- workshop/main.cpp
+++ workshop/main.cpp
@@ -6,15 +6,60 @@
 #include <steam/steam_api_flat.h>
 #include <optional>
 
+/*struct steam_api_call
+{
+    SteamAPICall_t call = 0;
+
+    steam_api_call(SteamAPICall_t in)
+    {
+        call = in;
+    }
+};*/
+
+void free_query_handle(UGCQueryHandle_t* in)
+{
+    ISteamUGC* self = SteamAPI_SteamUGC();
+
+    printf("Freed\n");
+
+    SteamAPI_ISteamUGC_ReleaseQueryUGCRequest(self, *in);
+
+    delete in;
+}
+
+struct request_handle
+{
+    std::shared_ptr<UGCQueryHandle_t> handle;
+    SteamAPICall_t call = 0;
+
+    request_handle(UGCQueryHandle_t in) : handle(new UGCQueryHandle_t(in), free_query_handle)
+    {
+
+    }
+
+    void dispatch()
+    {
+        ISteamUGC* self = SteamAPI_SteamUGC();
+
+        SteamAPICall_t result = SteamAPI_ISteamUGC_SendQueryUGCRequest(self, *handle);
+
+        printf("Dispatched\n");
+
+        call = result;
+    }
+};
+
 struct steam_api
 {
     steady_timer last_poll;
     std::vector<uint64_t> published_items;
     uint32_t appid = 0;
-    uint64_t steam_id = 0;
     uint32_t account_id = 0;
+    uint64_t steam_id = 0;
 
-    std::optional<UGCQueryHandle_t> current_query;
+    bool once = false;
+
+    std::optional<request_handle> current_query;
 
     steam_api()
     {
@@ -35,11 +80,111 @@ struct steam_api
 
     void poll()
     {
-        if(last_poll.get_elapsed_time_s() > 5)
+        #if 0
+        HSteamPipe hSteamPipe = SteamAPI_GetHSteamPipe(); // See also SteamGameServer_GetHSteamPipe()
+        SteamAPI_ManualDispatch_RunFrame(hSteamPipe);
+        CallbackMsg_t callback;
+        while(SteamAPI_ManualDispatch_GetNextCallback( hSteamPipe, &callback))
         {
-            UGCQueryHandle_t current_apps = SteamUGC()->CreateQueryUserUGCRequest(account_id, k_EUserUGCList_Published, k_EUGCMatchingUGCType_All, k_EUserUGCListSortOrder_CreationOrderDesc, appid, appid, 1);
+            printf("Callback\n");
+
+            // Check for dispatching API call results
+            if( callback.m_iCallback == SteamAPICallCompleted_t::k_iCallback )
+            {
+                SteamAPICallCompleted_t *pCallCompleted = (SteamAPICallCompleted_t*)callback.m_pubParam;
+                void *pTmpCallResult = malloc(callback.m_cubParam);
+                bool bFailed;
+
+                if(SteamAPI_ManualDispatch_GetAPICallResult(hSteamPipe, pCallCompleted->m_hAsyncCall, pTmpCallResult, callback.m_cubParam, callback.m_iCallback, &bFailed))
+                {
+                    // Dispatch the call result to the registered handler(s) for the
+                    // call identified by pCallCompleted->m_hAsyncCall
+
+                    printf("Got a steam callback result\n");
+                }
+
+                free(pTmpCallResult);
+            }
+            else if(callback.m_iCallback == SteamUGCQueryCompleted_t::k_iCallback)
+            {
+                SteamUGCQueryCompleted_t* pCallCompleted = (SteamUGCQueryCompleted_t*)callback.m_pubParam;
+
+                void *pTmpCallResult = malloc(callback.m_cubParam);
+
+                bool bFailed;
+
+                /*if(SteamAPI_ManualDispatch_GetAPICallResult(hSteamPipe, pCallCompleted->m_hAsyncCall, pTmpCallResult, callback.m_cubParam, callback.m_iCallback, &bFailed))
+                {
+                    // Dispatch the call result to the registered handler(s) for the
+                    // call identified by pCallCompleted->m_hAsyncCall
+
+                    printf("Got a UGC callback result\n");
+                }*/
+
+                printf("Ugc call?\n");
+
+                free(pTmpCallResult);
+            }
+            else
+            {
+                // Look at callback.m_iCallback to see what kind of callback it is,
+                // and dispatch to appropriate handler(s)
+            }
+
+            SteamAPI_ManualDispatch_FreeLastCallback(hSteamPipe);
+        }
+        #endif // 0
+
+        //if(last_poll.get_elapsed_time_s() > 5)
+        if(!once)
+        {
+            ISteamUGC* ugc = SteamAPI_SteamUGC();
+
+            UGCQueryHandle_t ugchandle = SteamAPI_ISteamUGC_CreateQueryUserUGCRequest(ugc, account_id, k_EUserUGCList_Published, k_EUGCMatchingUGCType_All, k_EUserUGCListSortOrder_CreationOrderDesc, appid, appid, 1);
+
+            SteamAPI_ISteamUGC_SetReturnOnlyIDs(ugc, ugchandle, true);
+            SteamAPI_ISteamUGC_SetReturnKeyValueTags(ugc, ugchandle, true);
+
+            request_handle handle(ugchandle);
 
             last_poll.restart();
+
+            current_query = std::move(handle);
+            current_query.value().dispatch();
+
+            once = true;
+        }
+
+        SteamAPI_RunCallbacks();
+
+        ISteamUtils* utils = SteamAPI_SteamUtils();
+
+        if(current_query.has_value())
+        {
+            SteamAPICall_t call = current_query.value().call;
+
+            /*bool failed = false;
+            std::cout << "Finished? " << SteamAPI_ISteamUtils_IsAPICallCompleted(utils, call, &failed) << std::endl;
+
+            if(failed)
+            {
+                std::cout << "Failed\n";
+            }*/
+
+            bool failed = false;
+            if(SteamAPI_ISteamUtils_IsAPICallCompleted(utils, call, &failed))
+            {
+                if(!failed)
+                {
+                    SteamUGCQueryCompleted_t completed;
+
+                    SteamAPI_ISteamUtils_GetAPICallResult(utils, call, &completed, sizeof(completed), SteamUGCQueryCompleted_t::k_iCallback, &failed);
+
+                    assert(!failed);
+
+                    printf("Done\n");
+                }
+            }
         }
     }
 
@@ -76,6 +221,8 @@ int main()
     while(!win.should_close())
     {
         win.poll();
+
+        steam.poll();
 
         vec2i window_size = win.get_window_size();
 
