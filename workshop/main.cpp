d093db914807a84ad0c92c66e5a37e43e2f9c705
20k <icedshot@gmail.com>
2022-01-15 12:17:34 +0000

    beginning of workshop subproject

---
+++ workshop/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <toolkit/render_window.hpp>
+#include <imgui/misc/freetype/imgui_freetype.h>
+
+int main()
+{
+    render_settings sett;
+    sett.width = 800;
+    sett.height = 600;
+    sett.no_double_buffer = true;
+    sett.is_srgb = true;
+
+    render_window win(sett, "Geodesics");
+
+    ImFontAtlas* atlas = ImGui::GetIO().Fonts;
+    atlas->FontBuilderFlags = ImGuiFreeTypeBuilderFlags_LCD | ImGuiFreeTypeBuilderFlags_FILTER_DEFAULT | ImGuiFreeTypeBuilderFlags_LoadColor;
+
+    ImFontConfig font_cfg;
+    font_cfg.GlyphExtraSpacing = ImVec2(0, 0);
+    font_cfg.FontBuilderFlags = ImGuiFreeTypeBuilderFlags_LCD | ImGuiFreeTypeBuilderFlags_FILTER_DEFAULT | ImGuiFreeTypeBuilderFlags_LoadColor;
+
+    ImGuiIO& io = ImGui::GetIO();
+
+    io.Fonts->Clear();
+    io.Fonts->AddFontFromFileTTF("VeraMono.ttf", 14, &font_cfg);
+
+
+    return 0;
+}
