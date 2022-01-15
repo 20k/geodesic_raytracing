fedc5e070662cd1b3a92488c6c72e25a9d05a2e5
20k <icedshot@gmail.com>
2022-01-15 14:36:35 +0000

    more steam api callbacks

--- workshop/main.cpp
+++ workshop/main.cpp
@@ -47,6 +47,45 @@ struct request_handle
 
         call = result;
     }
+
+    void query_details(int num)
+    {
+        ISteamUtils* utils = SteamAPI_SteamUtils();
+        ISteamUGC* ugc = SteamAPI_SteamUGC();
+
+        for(int i=0; i < num; i++)
+        {
+            SteamUGCDetails_t details;
+
+            if(SteamAPI_ISteamUGC_GetQueryUGCResult(ugc, *handle, i, &details))
+            {
+                std::string name(&details.m_rgchTitle[0], &details.m_rgchTitle[k_cchPublishedDocumentTitleMax - 1]);
+            }
+        }
+
+    }
+
+    bool poll()
+    {
+        ISteamUtils* utils = SteamAPI_SteamUtils();
+
+        bool failed = false;
+        if(SteamAPI_ISteamUtils_IsAPICallCompleted(utils, call, &failed))
+        {
+            if(!failed)
+            {
+                SteamUGCQueryCompleted_t completed;
+
+                SteamAPI_ISteamUtils_GetAPICallResult(utils, call, &completed, sizeof(completed), SteamUGCQueryCompleted_t::k_iCallback, &failed);
+
+                assert(!failed);
+
+                query_details(completed.m_unNumResultsReturned);
+            }
+        }
+
+        return false;
+    }
 };
 
 struct steam_api
@@ -80,61 +119,6 @@ struct steam_api
 
     void poll()
     {
-        #if 0
-        HSteamPipe hSteamPipe = SteamAPI_GetHSteamPipe(); // See also SteamGameServer_GetHSteamPipe()
-        SteamAPI_ManualDispatch_RunFrame(hSteamPipe);
-        CallbackMsg_t callback;
-        while(SteamAPI_ManualDispatch_GetNextCallback( hSteamPipe, &callback))
-        {
-            printf("Callback\n");
-
-            // Check for dispatching API call results
-            if( callback.m_iCallback == SteamAPICallCompleted_t::k_iCallback )
-            {
-                SteamAPICallCompleted_t *pCallCompleted = (SteamAPICallCompleted_t*)callback.m_pubParam;
-                void *pTmpCallResult = malloc(callback.m_cubParam);
-                bool bFailed;
-
-                if(SteamAPI_ManualDispatch_GetAPICallResult(hSteamPipe, pCallCompleted->m_hAsyncCall, pTmpCallResult, callback.m_cubParam, callback.m_iCallback, &bFailed))
-                {
-                    // Dispatch the call result to the registered handler(s) for the
-                    // call identified by pCallCompleted->m_hAsyncCall
-
-                    printf("Got a steam callback result\n");
-                }
-
-                free(pTmpCallResult);
-            }
-            else if(callback.m_iCallback == SteamUGCQueryCompleted_t::k_iCallback)
-            {
-                SteamUGCQueryCompleted_t* pCallCompleted = (SteamUGCQueryCompleted_t*)callback.m_pubParam;
-
-                void *pTmpCallResult = malloc(callback.m_cubParam);
-
-                bool bFailed;
-
-                /*if(SteamAPI_ManualDispatch_GetAPICallResult(hSteamPipe, pCallCompleted->m_hAsyncCall, pTmpCallResult, callback.m_cubParam, callback.m_iCallback, &bFailed))
-                {
-                    // Dispatch the call result to the registered handler(s) for the
-                    // call identified by pCallCompleted->m_hAsyncCall
-
-                    printf("Got a UGC callback result\n");
-                }*/
-
-                printf("Ugc call?\n");
-
-                free(pTmpCallResult);
-            }
-            else
-            {
-                // Look at callback.m_iCallback to see what kind of callback it is,
-                // and dispatch to appropriate handler(s)
-            }
-
-            SteamAPI_ManualDispatch_FreeLastCallback(hSteamPipe);
-        }
-        #endif // 0
-
         //if(last_poll.get_elapsed_time_s() > 5)
         if(!once)
         {
@@ -157,33 +141,11 @@ struct steam_api
 
         SteamAPI_RunCallbacks();
 
-        ISteamUtils* utils = SteamAPI_SteamUtils();
-
         if(current_query.has_value())
         {
-            SteamAPICall_t call = current_query.value().call;
-
-            /*bool failed = false;
-            std::cout << "Finished? " << SteamAPI_ISteamUtils_IsAPICallCompleted(utils, call, &failed) << std::endl;
-
-            if(failed)
+            if(current_query->poll())
             {
-                std::cout << "Failed\n";
-            }*/
-
-            bool failed = false;
-            if(SteamAPI_ISteamUtils_IsAPICallCompleted(utils, call, &failed))
-            {
-                if(!failed)
-                {
-                    SteamUGCQueryCompleted_t completed;
-
-                    SteamAPI_ISteamUtils_GetAPICallResult(utils, call, &completed, sizeof(completed), SteamUGCQueryCompleted_t::k_iCallback, &failed);
-
-                    assert(!failed);
-
-                    printf("Done\n");
-                }
+                current_query = std::nullopt;
             }
         }
     }
