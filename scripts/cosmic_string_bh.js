468a46213530c66bacedd6795854aea9f0c76d75
20k <icedshot@gmail.com>
2022-01-25 20:34:15 +0000

    dynamic cosmic string black hole

--- scripts/cosmic_string_bh.js
+++ scripts/cosmic_string_bh.js
@@ -1,14 +1,17 @@
 function cosmic_string(t, r, theta, phi)
 {
+	$cfg.rs.$default = 1;
+	$cfg.B.$default = 0.3;
+	
     var c = 1;
 
-    var rs = 1;
+    var rs = $cfg.rs;
 
     var dt = -(1 - rs/r) * c * c;
     var dr = 1 / (1 - rs/r);
     var dtheta = r * r;
 
-    var B = 0.3;
+    var B = $cfg.B;
     var dphi = r * r * B * B * CMath.sin(theta) * CMath.sin(theta);
 
     return [dt, dr, dtheta, dphi];
