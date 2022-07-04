0ad8d2d8301b03cbf186533b298a9b9e29274447
20k <icedshot@gmail.com>
2022-07-04 14:05:37 +0100

    build accel structure

--- triangle_manager.hpp
+++ triangle_manager.hpp
@@ -219,9 +219,65 @@ namespace triangle_rendering
         cl::buffer memory;
         cl::buffer memory_count;
 
+        vec3i offset_size = {128, 128, 128};
+        float offset_width = 20;
+
         acceleration(cl::context& ctx) : offsets(ctx), counts(ctx), memory(ctx), memory_count(ctx)
         {
+            memory_count.alloc(sizeof(cl_int));
 
+            int cells = offset_size.x() * offset_size.y() * offset_size.z();
+
+            offsets.alloc(sizeof(cl_int) * cells);
+            counts.alloc(sizeof(cl_int) * cells);
+            memory.alloc(1024 * 1024 * 128 * sizeof(cl_int));
+        }
+
+        void build(cl::command_queue& cqueue, manager& tris)
+        {
+            memory_count.set_to_zero(cqueue);
+
+            {
+                cl::args aclear;
+                aclear.push_back(counts);
+                aclear.push_back(offset_size.x());
+
+                cqueue.exec("clear_accel_counts", aclear, {offset_size.x(), offset_size.y(), offset_size.z()}, {8, 8, 1});
+            }
+
+            {
+                cl::args count_args;
+                count_args.push_back(tris.fill_points);
+                count_args.push_back(tris.fill_point_count);
+                count_args.push_back(counts);
+                count_args.push_back(offset_width);
+                count_args.push_back(offset_size.x());
+
+                cqueue.exec("generate_acceleration_counts", count_args, {tris.fill_point_count}, {256});
+            }
+
+            {
+                cl::args accel;
+                accel.push_back(offsets);
+                accel.push_back(counts);
+                accel.push_back(offset_size.x());
+                accel.push_back(memory_count);
+
+                cqueue.exec("alloc_acceleration", accel, {offset_size.x(), offset_size.y(), offset_size.z()}, {8, 8, 1});
+            }
+
+            {
+                cl::args gen;
+                gen.push_back(tris.fill_points);
+                gen.push_back(tris.fill_point_count);
+                gen.push_back(offsets);
+                gen.push_back(counts);
+                gen.push_back(memory);
+                gen.push_back(offset_width);
+                gen.push_back(offset_size.x());
+
+                cqueue.exec("generate_acceleration_data", gen, {tris.fill_point_count}, {256});
+            }
         }
     };
 }
