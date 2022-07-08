9acb053780e18bd495a45f8fdc59e08e2c253b2c
20k <icedshot@gmail.com>
2022-07-08 20:33:59 +0100

    beginning of tinyobjloader support

--- triangle_manager.cpp
+++ triangle_manager.cpp
@@ -1,5 +1,6 @@
 #include "triangle_manager.hpp"
 #include "print.hpp"
+#include <tinyobjloader/tiny_obj_loader.h>
 
 struct sub_point
 {
@@ -95,6 +96,89 @@ std::vector<subtriangle> triangulate_those_bigger_than(const std::vector<triangl
 }
 
 std::shared_ptr<triangle_rendering::object> triangle_rendering::manager::make_new()
+{
+    std::shared_ptr<object> obj = std::make_shared<object>();
+
+    cpu_objects.push_back(obj);
+
+    return obj;
+}
+
+std::vector<triangle> load_tris_from_model(const std::string& model_name)
+{
+    tinyobj::ObjReaderConfig reader_config;
+    reader_config.mtl_search_path = "./"; // Path to material files
+
+    tinyobj::ObjReader reader;
+
+    if(!reader.ParseFromFile(model_name, reader_config))
+    {
+        if(!reader.Error().empty())
+        {
+            std::cerr << "TinyObjReader: " << reader.Error();
+        }
+
+        throw std::runtime_error("Could not load object " + model_name);
+    }
+
+    if(!reader.Warning().empty())
+    {
+        std::cout << "TinyObjReader: " << reader.Warning();
+    }
+
+    auto& attrib = reader.GetAttrib();
+    auto& shapes = reader.GetShapes();
+    auto& materials = reader.GetMaterials();
+
+    // Loop over shapes
+    //for(size_t s = 0; s < shapes.size(); s++)
+
+    for(const tinyobj::shape_t& s : shapes)
+    {
+        // Loop over faces(polygon)
+        size_t index_offset = 0;
+
+        for(size_t fv : s.mesh.num_face_vertices)
+        {
+            // Loop over vertices in the face.
+            for(size_t v = 0; v < fv; v++)
+            {
+                // access to vertex
+                tinyobj::index_t idx = s.mesh.indices[index_offset + v];
+                tinyobj::real_t vx = attrib.vertices[3*size_t(idx.vertex_index)+0];
+                tinyobj::real_t vy = attrib.vertices[3*size_t(idx.vertex_index)+1];
+                tinyobj::real_t vz = attrib.vertices[3*size_t(idx.vertex_index)+2];
+
+                // Check if `normal_index` is zero or positive. negative = no normal data
+                /*if(idx.normal_index >= 0)
+                {
+                    tinyobj::real_t nx = attrib.normals[3*size_t(idx.normal_index)+0];
+                    tinyobj::real_t ny = attrib.normals[3*size_t(idx.normal_index)+1];
+                    tinyobj::real_t nz = attrib.normals[3*size_t(idx.normal_index)+2];
+                }
+
+                // Check if `texcoord_index` is zero or positive. negative = no texcoord data
+                if(idx.texcoord_index >= 0)
+                {
+                    tinyobj::real_t tx = attrib.texcoords[2*size_t(idx.texcoord_index)+0];
+                    tinyobj::real_t ty = attrib.texcoords[2*size_t(idx.texcoord_index)+1];
+                }*/
+
+                // Optional: vertex colors
+                // tinyobj::real_t red   = attrib.colors[3*size_t(idx.vertex_index)+0];
+                // tinyobj::real_t green = attrib.colors[3*size_t(idx.vertex_index)+1];
+                // tinyobj::real_t blue  = attrib.colors[3*size_t(idx.vertex_index)+2];
+            }
+
+            index_offset += fv;
+
+            // per-face material
+            //shapes[s].mesh.material_ids[f];
+        }
+    }
+}
+
+std::shared_ptr<triangle_rendering::object> triangle_rendering::manager::make_new(const std::string& model_name)
 {
     std::shared_ptr<object> obj = std::make_shared<object>();
 
