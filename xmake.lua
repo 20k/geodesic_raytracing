771f6dbed9e5b563638dd9a0e9bb92b55a7ad1f6
20k <icedshot@gmail.com>
2023-11-19 00:22:18 +0000

    update build system

--- xmake.lua
+++ xmake.lua
@@ -6,66 +6,73 @@ add_requires("freetype")
 add_requires("openssl", "opengl", "glfw", "glew", "sfml")
 add_requires("boost", {configs={filesystem=true}})
 
-target("main")
+add_files("workshop/steam_ugc_manager.cpp")
+add_files("deps/imgui/backends/imgui_impl_glfw.cpp")
+add_files("deps/imgui/backends/imgui_impl_opengl3.cpp")
+add_files("deps/imgui/misc/freetype/imgui_freetype.cpp")
+add_files("deps/imgui/misc/cpp/imgui_stdlib.cpp")
+add_files("deps/imgui/imgui.cpp")
+add_files("deps/imgui/imgui_draw.cpp")
+add_files("deps/imgui/imgui_tables.cpp")
+add_files("deps/imgui/imgui_widgets.cpp")
+add_files("deps/libfastcl/fastcl/cl.cpp")
+add_files("deps/quickjs/cutils.c")
+add_files("deps/quickjs/libbf.c")
+add_files("deps/quickjs/libregexp.c")
+add_files("deps/quickjs/libunicode.c")
+add_files("deps/quickjs/quickjs.c")
+add_files("deps/quickjs_cpp/quickjs_cpp.cpp")
+add_files("deps/tinyobjloader/tiny_obj_loader.cc")
+add_files("deps/toolkit/base_serialisables.cpp")
+add_files("deps/toolkit/clipboard.cpp")
+add_files("deps/toolkit/clock.cpp")
+add_files("deps/toolkit/fs_helpers.cpp")
+add_files("deps/toolkit/opencl.cpp")
+add_files("deps/toolkit/render_window.cpp")
+add_files("deps/toolkit/render_window_glfw.cpp")
+add_files("deps/toolkit/texture.cpp")
+add_includedirs("./deps")
+add_includedirs("./deps/imgui")
+add_includedirs("./deps/steamworks_sdk_153a/sdk/public")
+add_includedirs("./deps/steamworks_sdk_153a/sdk/public/steam")
+set_languages("c99", "cxx23")
+add_defines("IMGUI_IMPL_OPENGL_LOADER_GLEW",
+"SUBPIXEL_FONT_RENDERING",
+"SFML_STATIC",
+"GLEW_STATIC",
+"GRAPHITE2_STATIC",
+"CL_TARGET_OPENCL_VERSION=220",
+"IMGUI_ENABLE_FREETYPE",
+"CONFIG_VERSION=\"\"",
+"CONFIG_BIGNUM",
+"NO_SERIALISE_RATELIMIT",
+"FAST_CL")
+
+add_packages("openssl", "opengl", "glfw", "glew", "freetype", "sfml")
+
+set_optimize("fastest")
+
+add_linkdirs("deps/steamworks_sdk_153a/sdk/redistributable_bin/win64/")
+add_links("steam_api64")
+add_links("imm32")
+
+--add_packages("boost") causes compilation to fail
+add_ldflags("-l:libboost_filesystem-mt.a")
+
+if is_plat("mingw") then
+    add_ldflags("-static -static-libstdc++")
+end
+
+target("swt")
+    set_kind("binary")
+    add_files("workshop/*.cpp")
+    add_defines("NO_OPENCL")
+    
+target("RelativityWorkshop")
     set_kind("binary")
     add_files("main.cpp")
     add_files("*.cpp")
-    add_files("workshop/steam_ugc_manager.cpp")
-    add_files("deps/imgui/backends/imgui_impl_glfw.cpp")
-    add_files("deps/imgui/backends/imgui_impl_opengl3.cpp")
-    add_files("deps/imgui/misc/freetype/imgui_freetype.cpp")
-    add_files("deps/imgui/imgui.cpp")
-    add_files("deps/imgui/imgui_draw.cpp")
-    add_files("deps/imgui/imgui_tables.cpp")
-    add_files("deps/imgui/imgui_widgets.cpp")
-    add_files("deps/libfastcl/fastcl/cl.cpp")
-    add_files("deps/quickjs/cutils.c")
-    add_files("deps/quickjs/libbf.c")
-    add_files("deps/quickjs/libregexp.c")
-    add_files("deps/quickjs/libunicode.c")
-    add_files("deps/quickjs/quickjs.c")
-    add_files("deps/quickjs_cpp/quickjs_cpp.cpp")
-    add_files("deps/tinyobjloader/tiny_obj_loader.cc")
-    add_files("deps/toolkit/base_serialisables.cpp")
-    add_files("deps/toolkit/clipboard.cpp")
-    add_files("deps/toolkit/clock.cpp")
-    add_files("deps/toolkit/fs_helpers.cpp")
-    add_files("deps/toolkit/opencl.cpp")
-    add_files("deps/toolkit/render_window.cpp")
-    add_files("deps/toolkit/render_window_glfw.cpp")
-    add_files("deps/toolkit/texture.cpp")
-    add_includedirs("./deps")
-    add_includedirs("./deps/imgui")
-    add_includedirs("./deps/steamworks_sdk_153a/sdk/public")
-    add_includedirs("./deps/steamworks_sdk_153a/sdk/public/steam")
-    set_languages("c99", "cxx23")
-    add_defines("IMGUI_IMPL_OPENGL_LOADER_GLEW",
-    "SUBPIXEL_FONT_RENDERING",
-    "SFML_STATIC",
-    "GLEW_STATIC",
-    "GRAPHITE2_STATIC",
-    "CL_TARGET_OPENCL_VERSION=220",
-    "IMGUI_ENABLE_FREETYPE",
-    "CONFIG_VERSION=\"\"",
-    "CONFIG_BIGNUM",
-    "NO_SERIALISE_RATELIMIT",
-    "FAST_CL")
-
-    add_packages("openssl", "opengl", "glfw", "glew", "freetype", "sfml")
-
-    set_optimize("fastest")
     
-    add_linkdirs("deps/steamworks_sdk_153a/sdk/redistributable_bin/win64/")
-    add_links("steam_api64")
-    add_links("imm32")
-    
-    --add_packages("boost") causes compilation to fail
-    add_ldflags("-l:libboost_filesystem-mt.a")
-    
-    if is_plat("mingw") then
-        add_ldflags("-static -static-libstdc++")
-    end
-
 --
 -- If you want to known more usage about xmake, please see https://xmake.io
 --
